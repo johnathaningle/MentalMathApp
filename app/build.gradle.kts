e23cd528034626c929404d83dff2fd2181fa917f
John Ingle <>
2026-06-07 16:14:58 -0400

    Create github release and signing logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,24 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        val releaseStoreFile = project.findProperty("RELEASE_STORE_FILE") as? String
+        if (!releaseStoreFile.isNullOrEmpty()) {
+            create("release") {
+                storeFile = file(releaseStoreFile)
+                storePassword = project.findProperty("RELEASE_STORE_PASSWORD") as? String ?: ""
+                keyAlias = project.findProperty("RELEASE_KEY_ALIAS") as? String ?: ""
+                keyPassword = project.findProperty("RELEASE_KEY_PASSWORD") as? String ?: ""
+            }
+        }
+    }
+
     buildTypes {
         release {
+            val releaseStoreFile = project.findProperty("RELEASE_STORE_FILE") as? String
+            if (!releaseStoreFile.isNullOrEmpty()) {
+                signingConfig = signingConfigs.getByName("release")
+            }
             optimization {
                 enable = false
             }
